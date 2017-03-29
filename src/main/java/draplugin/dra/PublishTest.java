5dc9873d198b7fe7c38f9fbdcb53d76ab9444707
Xunrong Li <xunrongli@us.ibm.com>
2017-03-29 16:43:48 -0500

    update publishtestresult console output url

--- src/main/java/draplugin/dra/PublishTest.java
+++ src/main/java/draplugin/dra/PublishTest.java
@@ -337,7 +337,7 @@ public class PublishTest extends AbstractDevOpsAction implements SimpleBuildStep
         url = url.replace("{build_artifact}", URLEncoder.encode(this.applicationName, "UTF-8").replaceAll("\\+", "%20"));
         url = url.replace("{build_id}", buildNumber);
         this.dlmsUrl = url;
-        String link = chooseControlCenterUrl(env) + "buildverification?orgName=" + URLEncoder.encode(this.orgName, "UTF-8") + "&toolchainId=" + this.toolchainName;
+        String link = chooseControlCenterUrl(env) + "deploymentrisk?orgName=" + URLEncoder.encode(this.orgName, "UTF-8") + "&toolchainId=" + this.toolchainName;
 
         // get the Bluemix token
         try {
